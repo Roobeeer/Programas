9aa4287d7fe53c54537d02c43bfe7eb4d5bd0b3b
Rober <furybtw1@gmail.com>
2024-01-05 23:51:20 +0100

    main

---
+++ SumarImparesYPares.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import javax.swing.*;
+
+class SumarImparesYPares {
+    private static void main(String[]args){
+    String numero =JOptionPane.showInputDialog("Introduce un numero");
+    
+    }
+}
