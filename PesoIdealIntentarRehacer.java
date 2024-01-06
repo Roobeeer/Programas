4c14834cfd32b4865c0db0b20faca6785aa20eb2
Rober <furybtw1@gmail.com>
2024-01-06 21:18:30 +0100

    main

--- PesoIdealIntentarRehacer.java
+++ PesoIdealIntentarRehacer.java
@@ -1,28 +1,26 @@
 import javax.swing.*;
 
-public class PesoIdealIntentarRehacer {
+class PesoIdealIntentarRehacer{
     public static void main(String[]args){
-        String genero ="";
+        String genero = "";
 
         do{
-            genero = JOptionPane.showInputDialog("Introduce tu género(H/M)");
+        genero = JOptionPane.showInputDialog("Introduce tu genero(H/M)");
 
         }while(genero.equalsIgnoreCase("H")==false && genero.equalsIgnoreCase("M")==false);
 
-            int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura en cm"));
+        int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura"));
 
-            int pesoideal=0;
-            
-            if(genero.equalsIgnoreCase("H")){
+        int pesoideal=0;
 
-                pesoideal = altura-110;
-
-            }else if(genero.equalsIgnoreCase("M")){
-
-                pesoideal = altura-120;
-            }
-            System.out.println("Tu peso ideal es : "+pesoideal +" kg");
-
+        if(genero.equalsIgnoreCase("F")){
+            pesoideal = altura-110;
+        }else if(genero.equalsIgnoreCase("M")){
+            pesoideal = altura-120;
         }
+            System.out.println("Tu peso ideal es  : "+ pesoideal +" kg");
+
+        
+
     }
-    
+}
