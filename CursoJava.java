5f2a3ae77ce1cb42ef7c08fd0fd7ddaff36adc54
Rober <furybtw1@gmail.com>
2024-01-06 19:51:58 +0100

    main

--- CursoJava.java
+++ CursoJava.java
@@ -1,4 +1,4 @@
-import javax.swing.JOptionPane;
+/*import javax.swing.JOptionPane;
 public class CursoJava {
     public static void main(String[]args){
         String nombre = "Rober";
@@ -39,5 +39,23 @@ public class CursoJava {
 
 
 
+    }
+}
+*/
+import javax.swing.*;
+public class CursoJava {
+    public static void main(String[]args){
+        //BUCLE WHILE
+        String clave = "Hola";
+        String pass = "";
+
+        while(clave.equals(pass)==false){
+            pass = JOptionPane.showInputDialog("Introduce la contraseña por favor");
+
+            if(clave.equals(pass)==false){
+                System.out.println("Contraseña incorrecta, inténtelo de nuevo");
+            }
+        }
+        System.out.println("Contraseña correcta. Acceso permitido");
     }
 }
