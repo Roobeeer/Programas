b63bc17ee9a25de99786cf83c94b335e4b05f019
Roberto <furybtw1@gmail.com>
2024-01-09 00:01:52 +0100

    main

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+        public static void main(String[]args){
+            int array[]={5, 38, -15, 92, 71, 34, 21, 51, 52, 654, 61, 646, 76, 98};
+            
+
+            for(int i = 0;i<array.length;i++){
+                System.out.println("Valor del índice "+i + " es = "+array[i]);
+
+
+            }
+        }
+
+}
