6cdefe0cac7c47807be2c2b45fe6537ad78b001d
Roberto <furybtw1@gmail.com>
2024-01-12 17:17:56 +0100

    main

--- XelevadoY.java
+++ XelevadoY.java
@@ -1,27 +1,26 @@
-public class XelevadoY {
-    public static void main(String[]args){
-            System.out.println(Elevar(3, 2));
+import java.util.Scanner;
 
-            System.out.println(Elevar(-3, 2));
-            System.out.println(Elevar(3, -2));
-            System.out.println(Elevar(3, 0));
+public class XelevadoY{
+  public static void main(String[]args){
+    Scanner tec = new Scanner(System.in);
+    System.out.print("Introduce el valor de x --> ");
+    float x = tec.nextFloat();
+    System.out.print("Introduce el valor de y --> ");
+    float y = tec.nextFloat();
+    tec.close();
 
+    System.out.println("El resultado de " + x + " elevado a "+y+" es --> "+potencia(x,y));
+  }
+  //METODO PARA REALIZAR LA POTENCIA
+    public static float potencia(float x, float y){
+      float resultado = 1;
+      for(int i = 0; i<Math.abs(y);i++){
+        resultado = resultado*x;
+      }
 
+      if(y<0){
+        resultado = 1/resultado;
+      }
+      return resultado;
     }
-
-    public static float Elevar(float x, float y){
-        float resultado = 1;
-        for (int i= 1; i<=Math.abs(y);i++){
-            resultado = resultado*x;    
-        }
-
-        if(y<0){
-            resultado = 1/resultado;
-        }
-        return resultado;
- 
-
-
-    }
-    
 }
