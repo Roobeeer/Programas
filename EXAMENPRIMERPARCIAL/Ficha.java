6de20e67f41dd809c4208daeb4dab07e37bc21d9
Roberto <furybtw1@gmail.com>
2024-02-04 18:58:25 +0100

    main

---
+++ EXAMENPRIMERPARCIAL/Ficha.java
@@ -0,0 +1,16 @@
+package EXAMENPRIMERPARCIAL;
+import java.util.*;
+public class Ficha {
+    
+    public static void main(String[]args){
+        Scanner tec =  new Scanner(System.in);
+        System.out.println("*** Piedra, papel o tijeras ***");
+        System.out.print("Jugador 1 , introduce Piedra, papel o tijeras");
+        String jugador1 = tec.nextLine();
+        System.out.print("Jugador 2, introduce Piedra, papel o tijeras");
+        String jugador2 = tec.nextLine();
+        
+
+        
+}
+}
--- EXAMENPRIMERPARCIAL/ProblemaExamen.java
+++
@@ -1,5 +0,0 @@
-package EXAMENPRIMERPARCIAL;
-
-public class ProblemaExamen {
-    
-}
